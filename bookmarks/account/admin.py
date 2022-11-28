771ff59bbec87e2eeaff28d21f3c6bddeecd4a3a
dasubermanmind <ero33@drexel.edu>
2022-11-28 17:54:44 -0500

    added pillow

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Profile
+
+@admin.register
+class ProfileAdmin(Profile):
+    list_display = ['user', 'date_of_birth','photo']
+    raw_id_fields = ['user']
